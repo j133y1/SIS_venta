ef7ee6f7a05bd026d866914b85dbd833f1060954
j133y1 <jim_fj_dark@hotmail.com>
2015-12-20 21:29:00 -0400

    0.0.

--- SIS_venta/agregar vendedores.Designer.cs
+++ SIS_venta/agregar vendedores.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // agregar_vendedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 261);
+            this.Name = "agregar_vendedores";
             this.Text = "agregar_vendedores";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
